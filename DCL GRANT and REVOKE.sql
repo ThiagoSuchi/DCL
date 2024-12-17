c1eabf09ef99365b30be805f13aa0f2c11febbe5
Thiago Hens Suchi <166747903+ThiagoSuchi@users.noreply.github.com>
2024-12-17 10:44:53 -0400

    Update DCL GRANT and REVOKE.sql

--- DCL GRANT and REVOKE.sql
+++ DCL GRANT and REVOKE.sql
@@ -22,3 +22,24 @@ revoke insert, delete, select on biblioteca.* from aluno@'localhost';
 revoke all on biblioteca.* from aluno@'localhost';
 
 flush privileges;
+show grants for 'aluno'@'localhost';
+
+# 04 - Roles
+create role 'app_developer'@'localhost', 'app_read'@'localhost', 'app_write'@'localhost';
+
+grant all on biblioteca.* to 'app_developer'@'localhost';
+grant select on biblioteca.* to 'app_write'@'localhost';
+grant insert, update on biblioteca.* to 'app_read'@'localhost';
+
+create user 'dev1'@'localhost' identified by '.localhost#13';
+create user 'read_user1'@'localhost' identified by '.localhost#13';
+create user 'read_user2'@'localhost' identified by '.localhost#13';
+create user 'rw_user3'@'localhost' identified by '.localhost#13';
+
+grant 'app_developer'@'localhost' to 'dev1'@'localhost';
+grant 'app_read'@'localhost' to 'read_user1'@'localhost';
+grant 'app_read'@'localhost' to 'read_user2'@'localhost';
+grant 'app_read'@'localhost', 'app_write'@'localhost' to 'rw_user3'@'localhost';
+
+flush privileges;
+
